f1b8fcce61fc2485f32ef569f71e0b4a728726f5
Brandon Voravongsa <b.voravongsa@gmail.com>
2023-03-05 16:58:42 -0600

    first two finished?

--- hw4_1/p1-1.asm
+++ hw4_1/p1-1.asm
@@ -1,1 +1,11 @@
 // Write your assembly program for Problem 1 (a) #1 here.
+
+// Test ST instruction
+// 10000 sss ddd iiiii | ST Rd, Rs, imm | Mem[Rs + I(sign ext.)] <- Rd
+
+lbi r1, 4
+lbi r2, 2
+lbi r3, 0
+add r4, r1, r2
+st r4, r5, 5
+halt
--- hw4_1/p1-2.asm
+++ hw4_1/p1-2.asm
@@ -1,1 +1,12 @@
 // Write your assembly program for Problem 1 (a) #2 here.
+
+// Test ST instruction
+// 10000 sss ddd iiiii | ST Rd, Rs, imm | Mem[Rs + I(sign ext.)] <- Rd
+
+lbi r1, 6
+lbi r2, 9
+lbi r3, 0
+st r2, r2, 2
+add r2, r1, r2
+halt
+

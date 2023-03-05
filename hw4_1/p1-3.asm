98fff7ce830c23de975adea367a590d94802693e
Brandon Voravongsa <b.voravongsa@gmail.com>
2023-03-05 17:03:40 -0600

    part 2

--- hw4_1/p1-3.asm
+++ hw4_1/p1-3.asm
@@ -1,1 +1,12 @@
 // Write your assembly program for Problem 1 (a) #3 here.
+
+// Test ST instruction
+// 10000 sss ddd iiiii | ST Rd, Rs, imm | Mem[Rs + I(sign ext.)] <- Rd
+
+lbi r1, 9
+lbi r2, 8
+lbi r3, 9
+add r3, r1, r2
+st r3, r2, 7
+add r2, r3, r1
+halt

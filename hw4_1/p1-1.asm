ee17f6dd345256de09271b0526e028f378f9902d
aaaabhaypunjabiiii <56175235+aaaabhaypunjabiiii@users.noreply.github.com>
2023-03-05 19:21:47 -0600

    Update p1-1.asm

--- hw4_1/p1-1.asm
+++ hw4_1/p1-1.asm
@@ -3,6 +3,7 @@
 // Test ST instruction
 // 10000 sss ddd iiiii | ST Rd, Rs, imm | Mem[Rs + I(sign ext.)] <- Rd
 
+// Testing EX-EX forwarding, the result of the ADD (line 10) is forwarded to the ST(on line 11), since we're storing R1.
 lbi r1, 4
 lbi r2, 2
 lbi r3, 0
